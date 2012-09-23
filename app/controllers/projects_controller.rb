8fd7604440aac89321811e81db1a8cf82b56029b
roRisc <paulcu@gmail.com>
2012-09-23 13:01:07 -0400

    Adding updating projects functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,10 +15,23 @@ class ProjectsController < ApplicationController
       flash[:alert] = "Project has not been created."
       render action: "new" 
     end
-     
   end
   
   def show 
     @project = Project.find params[:id]
+  end
+  
+  def edit 
+    @project = Project.find params[:id]
+  end
+  
+  def update
+    @project = Project.find params[:id]
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice: "Project has been updated."  
+    else
+      flash[:alert] = "Project has not been updated."
+      render action: "edit"      
+    end
   end
 end
--- features/viewing_projects.feature
+++ features/viewing_projects.feature
@@ -1,4 +1,4 @@
-Feature: Viewing project
+Feature: Viewing projects
 	In order to assign tickets to a project
 	As a user
 	I want to be able to see a list of available projects
